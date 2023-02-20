4918117d7384a234390b9df502cd8df0cf0b8c04
sanket <wankhdes29@gmail.com>
2023-02-20 12:22:53 +0530

    starter code

---
+++ src/main/java/com/sanket/AppointmentBookingApplication.java
@@ -0,0 +1,13 @@
+package com.sanket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanket/AppointmentBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
