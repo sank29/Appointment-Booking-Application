3e691c65c719a7689b97c04abe01f2a47757b59e
sanket <wankhdes29@gmail.com>
2023-04-30 21:35:34 +0530

    fix: fix some code

--- src/main/java/com/sanket/service/PatientServiceImpl.java
+++ src/main/java/com/sanket/service/PatientServiceImpl.java
@@ -824,7 +824,7 @@ public class PatientServiceImpl implements PatientService, Runnable {
 			
 		}else {
 			
-			throw new PatientException("Patient not found");
+			throw new PatientException("Patient not found.");
 		}
 	}
 
