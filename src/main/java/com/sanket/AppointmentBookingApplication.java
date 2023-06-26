0d3fabbb38bd59a192cdd4933925e1c4378aad7d
sanket <wankhdes29@gmail.com>
2023-06-26 12:47:02 +0530

    fix: made book appointment block thread safe

--- src/main/java/com/sanket/AppointmentBookingApplication.java
+++ src/main/java/com/sanket/AppointmentBookingApplication.java
@@ -1,5 +1,6 @@
 package com.sanket;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +9,7 @@ public class AppointmentBookingApplication {
 
 	public static void main(String[] args) {
 		
-		SpringApplication.run(AppointmentBookingApplication.class, args); 
+		SpringApplication.run(AppointmentBookingApplication.class, args);
 		
 	}
 	
